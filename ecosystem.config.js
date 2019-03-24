3ccf876ef24830bb08618ee2a0390f32105a408e
Hhpon <624791164@qq.com>
2019-03-24 19:45:41 +0800

    change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,30 @@
 module.exports = {
-  apps : [{
-    name: 'quiz',
-    script: 'app.js',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: "quiz",
+      script: "app.js",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
     }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'hhp',
-      host : '47.94.210.206',
-      ref  : 'origin/master',
-      repo : 'git@github.com:Hhpon/newCheckout-back-end.git',
-      path : '/home/hhp/www/lsj_test/production',
-      ssh_options: 'StrictHostKeyChecking=no',
-      'post-deploy': 'npm install --registry=https://registry.npm.taobao.org && pm2 reload ecosystem.config.js --env production',
-      env:{
-        NODE_ENV: 'production'
+  deploy: {
+    production: {
+      user: "hhp",
+      host: "47.94.210.206",
+      port: "1221",
+      ref: "origin/master",
+      repo: "git@github.com:Hhpon/newCheckout-back-end.git",
+      path: "/home/hhp/www/lsj_test/production",
+      ssh_options: "StrictHostKeyChecking=no",
+      "post-deploy":
+        "npm install --registry=https://registry.npm.taobao.org && pm2 reload ecosystem.config.js --env production",
+      env: {
+        NODE_ENV: "production"
       }
     }
   }
